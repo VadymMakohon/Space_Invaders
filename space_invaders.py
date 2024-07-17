81c04ca9a3956d3fe714b751305778957ee93464
VadymMakohon <errorks7@gmail.com>
2024-07-17 09:07:57 +0200

    game loop is done

--- space_invaders.py
+++ space_invaders.py
@@ -7,6 +7,8 @@ pygame.init()
 SCREEN_WIDTH = 750
 SCREEN_HEIGHT = 700
 
+GREY = (29, 29, 27)
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Python Space Invanders")
 
@@ -18,6 +20,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    #Drawing
+    screen.fill(GREY)
 
     pygame.display.update()
     clock.tick(60)
