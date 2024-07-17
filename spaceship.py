7f32aa7735f00c21c0405048bc48e0ccf563952a
VadymMakohon <errorks7@gmail.com>
2024-07-17 14:28:23 +0200

    updated the spaceship and made a border for him on the screen

--- spaceship.py
+++ spaceship.py
@@ -1,11 +1,12 @@
 import pygame
 class Spaceship(pygame.sprite.Sprite):
-    def __inint__(self, screen_width, screen_height):
+    def __init__(self, screen_width, screen_height):
         super().__init__()
+        # self.offset = offset
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.image = pygame.image.load("Graphics/spaceship.png")
-        self.rect = self.omage.get_rect(midbottom = (self.screen_width/2, self.screen_height))
+        self.rect = self.image.get_rect(midbottom = (self.screen_width/2, self.screen_height))
         self.speed = 6
 
     def get_user_input(self):
@@ -19,3 +20,10 @@ class Spaceship(pygame.sprite.Sprite):
 
     def update(self):
         self.get_user_input()
+        self.canstrain_movement()
+
+    def canstrain_movement(self):
+        if self.rect.right > self.screen_width:
+            self.rect.right = self.screen_width
+        if self.rect.left < 0:
+            self.rect.left = 0
