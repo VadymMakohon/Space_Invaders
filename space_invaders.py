34cfbf7afeaf47ac7b804dff6b74bf65deaa1cde
VadymMakohon <errorks7@gmail.com>
2024-07-24 11:35:20 +0200

    added level attribute to the Game class and added level_up method to increment the level and reset the aliens

--- space_invaders.py
+++ space_invaders.py
@@ -1,6 +1,4 @@
-import pygame
-import sys
-import random
+import pygame, sys, random
 from game import Game
 
 pygame.init()
@@ -32,62 +30,64 @@ MYSTERYSHIP = pygame.USEREVENT + 1
 pygame.time.set_timer(MYSTERYSHIP, random.randint(4000, 8000))
 
 while True:
-	# Checking for events
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == SHOOT_LASER and game.run:
-			game.alien_shoot_laser()
-		if event.type == MYSTERYSHIP and game.run:
-			game.create_mystery_ship()
-			pygame.time.set_timer(MYSTERYSHIP, random.randint(4000, 8000))
+    # Checking for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == SHOOT_LASER and game.run:
+            game.alien_shoot_laser()
 
-	keys = pygame.key.get_pressed()
-	if keys[pygame.K_SPACE] and not game.run:
-		game.reset()
+        if event.type == MYSTERYSHIP and game.run:
+            game.create_mystery_ship()
+            pygame.time.set_timer(MYSTERYSHIP, random.randint(4000, 8000))
 
-	# Updating
-	if game.run:
-		game.spaceship_group.update()
-		game.move_aliens()
-		game.alien_lasers_group.update()
-		game.mystery_ship_group.update()
-		game.check_for_collisions()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and game.run == False:
+            game.reset()
 
-	# Drawing
-	screen.fill(GREY)
+    # Updating
+    if game.run:
+        game.spaceship_group.update()
+        game.move_aliens()
+        game.alien_lasers_group.update()
+        game.mystery_ship_group.update()
+        game.check_for_collisions()
 
-	# UI
-	pygame.draw.rect(screen, YELLOW, (10, 10, 780, 780), 2, 0, 60, 60, 60, 60)
-	pygame.draw.line(screen, YELLOW, (25, 730), (775, 730), 3)
+    # Drawing
+    screen.fill(GREY)
 
-	if game.run:
-		screen.blit(level_surface, (570, 740, 50, 50))
-	else:
-		screen.blit(game_over_surface, (570, 740, 50, 50))
+    # UI
+    pygame.draw.rect(screen, YELLOW, (10, 10, 780, 780), 2, 0, 60, 60, 60, 60)
+    pygame.draw.line(screen, YELLOW, (25, 730), (775, 730), 3)
 
-	x = 50
-	for life in range(game.lives):
-		screen.blit(game.spaceship_group.sprite.image, (x, 745))
-		x += 50
+    if game.run:
+        level_surface = font.render(f"LEVEL {str(game.level).zfill(2)}", False, YELLOW)
+        screen.blit(level_surface, (570, 740, 50, 50))
+    else:
+        screen.blit(game_over_surface, (570, 740, 50, 50))
 
-	screen.blit(score_text_surface, (50, 15, 50, 50))
-	formatted_score = str(game.score).zfill(5)
-	score_surface = font.render(formatted_score, False, YELLOW)
-	screen.blit(score_surface, (50, 40, 50, 50))
-	screen.blit(highscore_text_surface, (550, 15, 50, 50))
-	formatted_highscore = str(game.highscore).zfill(5)
-	highscore_surface = font.render(formatted_highscore, False, YELLOW)
-	screen.blit(highscore_surface, (625, 40, 50, 50))
+    x = 50
+    for life in range(game.lives):
+        screen.blit(game.spaceship_group.sprite.image, (x, 745))
+        x += 50
 
-	game.spaceship_group.draw(screen)
-	game.spaceship_group.sprite.lasers_group.draw(screen)
-	for obstacle in game.obstacles:
-		obstacle.blocks_group.draw(screen)
-	game.aliens_group.draw(screen)
-	game.alien_lasers_group.draw(screen)
-	game.mystery_ship_group.draw(screen)
+    screen.blit(score_text_surface, (50, 15, 50, 50))
+    formatted_score = str(game.score).zfill(5)
+    score_surface = font.render(formatted_score, False, YELLOW)
+    screen.blit(score_surface, (50, 40, 50, 50))
+    screen.blit(highscore_text_surface, (550, 15, 50, 50))
+    formatted_highscore = str(game.highscore).zfill(5)
+    highscore_surface = font.render(formatted_highscore, False, YELLOW)
+    screen.blit(highscore_surface, (625, 40, 50, 50))
 
-	pygame.display.update()
-	clock.tick(60)
+    game.spaceship_group.draw(screen)
+    game.spaceship_group.sprite.lasers_group.draw(screen)
+    for obstacle in game.obstacles:
+        obstacle.blocks_group.draw(screen)
+    game.aliens_group.draw(screen)
+    game.alien_lasers_group.draw(screen)
+    game.mystery_ship_group.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
